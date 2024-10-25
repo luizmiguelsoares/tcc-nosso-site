21683fa8e5e6a06258812e1404144dd0b4e533b4
Luizinhopaocomovo <luiz.miguel1406007@gmail.com.br>
2024-10-25 15:41:29 -0300

    fasequasecompleta

---
+++ src/pages/app/index.jsx
@@ -0,0 +1,41 @@
+import './index.scss';
+
+function App() {
+  return (
+    <div className="pagina-inicial">
+       <div className='cabecalho'>
+        <img src="" alt="" />
+        <h3>inicio</h3>
+         <h3>sobre nós</h3>
+          <h3>feedback</h3>    
+          <h3>contato</h3>
+          </div>
+          <div className='slogan'>
+            <h1>
+            Empresa Desenvolvedora de Web’s Sites
+            </h1>
+            <h3>
+            Expêriencia no Desenvolvimento de Web’s Sites para o Aprimoramento de seu Negócio
+            </h3>
+            <h3>
+            Sites 100% responsívos Desenvolvimento feito apartir de suas necessidades
+            </h3>
+
+            <div className=' texto1'>
+            <h1>INICIO</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br/> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br/> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br/> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br/> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br/> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br/> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor</p>
+            </div>
+            <div className='texto2'>
+              <h1>SOBRE NÓS</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br/> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br/> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br/> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br/> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br/> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br/> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor</p>
+            </div>
+          </div>
+    </div>
+  );
+}
+
+export default App;
