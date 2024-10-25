7ab1a340821631a5975b03e5cdd966689354afc2
Luizinhopaocomovo <luiz.miguel1406007@gmail.com.br>
2024-10-25 16:36:30 -0300

    1233

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -3,37 +3,47 @@ import './index.scss';
 function App() {
   return (
     <div className="pagina-inicial">
-       <div className='cabecalho'>
+      <div className='cabecalho'>
         <img src="" alt="" />
-        <h3>inicio</h3>
-         <h3>sobre nós</h3>
-          <h3>feedback</h3>    
-          <h3>contato</h3>
-          </div>
-          <div className='slogan'>
-            <h1>
-            Empresa Desenvolvedora de Web’s Sites
-            </h1>
-            <h3>
-            Expêriencia no Desenvolvimento de Web’s Sites para o Aprimoramento de seu Negócio
-            </h3>
-            <h3>
-            Sites 100% responsívos Desenvolvimento feito apartir de suas necessidades
-            </h3>
+        <h3>Início</h3>
+        <h3>sobre nós</h3>
+        <h3>feedback</h3>
+        <h3>contato</h3>
+      </div>
+      <div className='slogan'>
+        <h1>
+          Empresa Desenvolvedora de Web’s Sites
+        </h1>
+        <h3>
+          Expêriencia no Desenvolvimento de Web’s Sites para o Aprimoramento de seu Negócio
+        </h3>
+        <div className='seta'>
+        <img  src="./assets/images/seta-direita.png" alt="" />
 
-            <div className=' texto1'>
-            <h1>INICIO</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br/> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br/> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br/> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br/> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br/> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br/> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor</p>
-            </div>
-            <div className='texto2'>
-              <h1>SOBRE NÓS</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br/> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br/> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br/> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br/> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br/> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br/> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor</p>
-            </div>
-          </div>
+
+          <h4>
+            Sites 100% responsívos Desenvolvimento feito apartir de suas necessidades
+          </h4>
+        </div>
+        <div className=' texto1'>
+          <h1>INICIO</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br /> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br /> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor</p>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br /> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br /> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br /> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br /> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor</p>
+        </div>
+        <div className='texto2'>
+          <h1>SOBRE NÓS</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br /> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br /> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br /> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br /> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa consequuntur dolores <br /> odit cumque atque voluptas expedita! Dicta ipsum nesciunt obcaecati? <br /> Adipisci quo recusandae repudiandae officia dolorum eaque earum dolor</p>
+        </div>
+      </div>
+      <div className='texto3 '>
+        <h1>contate-nós</h1>
+        <img className='whatsap' src="./assets/images/whatsapp.png" alt="" />
+
+      </div>
     </div>
   );
 }
